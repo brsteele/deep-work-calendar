d271f834a31f9560d5af22c86dc8b8b7981d1f42
Brian Steele <brsteele2123@gmail.com>
2019-05-19 12:43:25 -0500

    changing calendar to not have dynamic height

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { debounce } from 'lodash';
 
 const Events: React.FunctionComponent = () => {
-  const [windowHeight, setWindowHeight] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(0);
   const [eventsContainerDimensions, setEventsContainerDimensions] = useState({
     height: 0,
     width: 0
   });
 
   const useHandleWindowResize = debounce(() => {
-    handleWindowResize(setWindowHeight);
+    handleWindowResize(setWindowWidth);
   }, 100);
 
-  const eventsRef: any = React.createRef();
+  const eventsRef: React.RefObject<HTMLDivElement> = React.createRef();
 
   useEffect(() => {
-    setWindowHeight(document.body.clientHeight);
+    setWindowWidth(document.body.clientWidth);
     window.addEventListener('resize', useHandleWindowResize);
     return () => {
       window.removeEventListener('resize', useHandleWindowResize);
@@ -23,21 +23,24 @@ const Events: React.FunctionComponent = () => {
   }, []);
 
   useEffect(() => {
-    const { height, width } = eventsRef.current.getBoundingClientRect();
+    const {
+      height,
+      width
+    } = (eventsRef.current as HTMLDivElement).getBoundingClientRect();
     setEventsContainerDimensions({ height, width });
-  }, [windowHeight]);
+  }, [windowWidth]);
 
   return (
     <div ref={eventsRef} className="days" id="events">
       <div className="event">
-        <div className="title">{windowHeight}</div>
+        <div className="title">{windowWidth}</div>
       </div>
     </div>
   );
 };
 
 const handleWindowResize = (callBack: (height: number) => void) => {
-  callBack(document.body.clientHeight);
+  callBack(document.body.clientWidth);
 };
 
 export default Events;
